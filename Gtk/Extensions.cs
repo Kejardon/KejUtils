e07099604294fe5dfe4e9acea386401bdab73ba5
Kejardon <Kejardon@gmail.com>
2020-11-27 13:20:06 -0800

    Forgot to commit this a while ago. A few utilities used in TabletopGenie

---
+++ Gtk/Extensions.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Gtk;
+
+namespace KejUtils
+{
+    public static partial class Extensions
+    {
+        public static bool GetPrev(this ListStore model, TreeIter iter, out TreeIter prevIter)
+        {
+            TreePath path = model.GetPath(iter);
+            int[] index = path.Indices;
+            if (index[index.Length - 1] > 0)
+            {
+                index[index.Length - 1]--;
+                TreePath prevPath = new TreePath(index);
+                model.GetIter(out prevIter, prevPath);
+                return true;
+            }
+            prevIter = default(TreeIter);
+            return false;
+        }
+        public static bool GetNext(this ListStore model, TreeIter iter, out TreeIter nextIter)
+        {
+            TreePath path = model.GetPath(iter);
+            int[] index = path.Indices;
+            index[index.Length - 1]++;
+            TreePath nextPath = new TreePath(index);
+            if (model.GetIter(out nextIter, nextPath))
+                return true;
+            return false;
+        }
+
+        public static Button Make(this Button button, EventHandler clickAction, string buttonName)
+        {
+            if (button == null)
+            {
+                if (buttonName != null)
+                    button = new Button(buttonName);
+                else
+                    button = new Button();
+            }
+            else
+            {
+                if (buttonName != null)
+                    button.Label = buttonName;
+            }
+            if (clickAction != null)
+                button.Clicked += clickAction;
+
+            return button;
+        }
+    }
+}
---
+++ StructTuple.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KejUtils
+{
+    public struct StructTuple<I,J> : IEquatable<StructTuple<I,J>>
+    {
+        readonly I First;
+        readonly J Second;
+
+        public StructTuple(I first, J second)
+        {
+            First = first;
+            Second = second;
+        }
+
+        public I A { get { return First; } }
+        public J B { get { return Second; } }
+
+        public override int GetHashCode()
+        {
+            return First.GetHashCode() ^ Second.GetHashCode();
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (!(obj is StructTuple<I, J>))
+            {
+                return false;
+            }
+            return Equals((StructTuple<I, J>)obj);
+        }
+
+        public bool Equals(StructTuple<I, J> other)
+        {
+            return other.First.Equals(First) && other.Second.Equals(Second);
+        }
+    }
+}
